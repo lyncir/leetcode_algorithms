d20ff0f82881773babe50ef9d7b0a4a2742dc1f6
lyncir <lyncir@gmail.com>
2016-10-10 15:04:57 +0800

    Reverse Integer

--- solution.py
+++ solution.py
@@ -13,3 +13,26 @@ def two_sum(nums, target):
 
         if other in other_nums:
             return [nums.index(num), nums.index(other, nums.index(num) + 1)]
+
+
+# 7. Reverse Integer
+def reverse(x):
+    '''
+    :type x: int
+    :rtype: int
+    '''
+    if x > 0:
+        num = reversed(str(x))
+        result = int(''.join(num))
+    elif x < 0:
+        num = reversed(str(abs(x)))
+        result = -int(''.join(num))
+    else:
+        result = x
+
+    # 32-bit integer overflow
+    if not (-2**32/2 <= result <= 2**32/2):
+        result = 0
+
+    return result
+        
