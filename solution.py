61ff09a78ac1b4c69b35913840e3ece051872a36
lyncir <lyncir@gmail.com>
2016-10-17 15:31:15 +0800

    and 128

--- solution.py
+++ solution.py
@@ -62,3 +62,29 @@ def is_palindrome(x):
         return True
     else:
         return False
+
+# 128. Longest Consecutive Sequence
+def longest_consecutive_seq(nums):
+    '''
+    :type nums: list
+    :rtype: int
+    '''
+    # 排除重复元素
+    nums = set(nums)
+
+    ans = 0
+
+    while nums:
+        l = r = nums.pop()
+
+        while l - 1 in nums:
+            l -= 1
+            nums.remove(l)
+
+        while r + 1 in nums:
+            r += 1
+            nums.remove(r)
+
+        ans = max(ans, r - l + 1)
+
+    return ans
